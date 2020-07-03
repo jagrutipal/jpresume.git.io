3d15beb7cff326b44c937d400c1cc0ec36451a8b
jagrutipal <66962041+jagrutipal@users.noreply.github.com>
2020-07-03 23:45:09 +0530

    upload website

---
+++ myscript.js
@@ -0,0 +1,9 @@
+function openSM() {
+    document.getElementById("mySidemenu").style.width = "450px";
+    document.getElementById("pg-content").style.marginleft = "450px";
+}
+
+function closeSM() {
+    document.getElementById("mySidemenu").style.width = "0";
+    document.getElementById("mySidemenu").style.marginleft = "0";
+}
